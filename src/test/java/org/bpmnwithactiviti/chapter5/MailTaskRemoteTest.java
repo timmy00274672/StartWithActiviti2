0e95c73b6a0402b0f3923ff2d01b46a8fe0fd08c
Yu-Ting Chen <timmy00274672@gmail.com>
2014-12-05 18:00:21 +0800

    Do the chapter 5 in Activiti In Action.

---
+++ src/test/java/org/bpmnwithactiviti/chapter5/MailTaskRemoteTest.java
@@ -0,0 +1,25 @@
+package org.bpmnwithactiviti.chapter5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MailTaskRemoteTest {
+
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule(
+			"activiti.cfg-mem-mail.xml");
+
+	@Test
+	@Deployment(resources = { "chapter5/testSimpleMail.bpmn20.xml" })
+	public void sendMailRemoteTest() throws Exception {
+		Map<String, Object> processVariables = new HashMap<String, Object>();
+		processVariables.put("name", "Miss Piggy");
+		activitiRule.getRuntimeService().startProcessInstanceByKey(
+				"simpleEmailProcess", processVariables);
+	}
+}
